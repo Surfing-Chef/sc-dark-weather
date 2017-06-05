aac44fd11eafdb19fa0f6c40b0f0ddf9625b6d2e
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-06-05 15:15:21 -0700

    Initialized the plugin with a comment block

---
+++ sc_dark_weather.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: SC Dark Weather
+Plugin URI: https://github.com/Surfing-Chef/Dark
+Description: Uses a wrapper to display a weather feed from Darksky.net
+Version: 1.2
+Author: Surfing-Chef
+Author URI: https://github.com/Surfing-Chef
+License: GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: sc-weather
+*/
