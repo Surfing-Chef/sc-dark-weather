158e02d3bd50fb90dc17a41a056f286acef69308
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-06-08 19:18:29 -0700

    Commented out delete code

--- uninstall.php
+++ uninstall.php
@@ -3,9 +3,15 @@
 // This is checking to make sure the plugin is being uninstalled by a Wordpress // Admin. This variable only contains a value if an Admin is activating the
 // uninstall script from inside Wordpress.
 
-if( !defined( 'WP_UNINSTALL_PLUGIN' ) )
-	exit(); // End the script if accessed by a non-admin
-
-delete_option( 'sc_defaults', 'sc_title');
+// if uninstall.php is not called by WordPress, die
+// if (!defined('WP_UNINSTALL_PLUGIN')) {
+//     die;
+// }
+//
+// delete_option( 'sc_defaults');
+// delete_option( 'sc_title');
+// delete_option( 'sc_api');
+// delete_option( 'sc_longitude');
+// delete_option( 'sc_latitude');
 
 ?>
