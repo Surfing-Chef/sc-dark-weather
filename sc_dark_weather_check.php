482d47db69f4a2b427f16fbef22b8b205679a459
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-06-13 16:55:38 -0700

    Created class to check data in WordPress database against plugin options, and widget requests

--- sc_dark_weather_check.php
+++ sc_dark_weather_check.php
@@ -1,6 +1,19 @@
 <?php
+/*
+* Description: Check Darksky data in WordPress database against plugin options,
+  and widget requests.
+   
+* Returns array containing:
+      file ( string )
+      token ( string )
+      long ( string )
+      lat ( string )
+      update( bool )
+*/
+
 // Import Functions
 require_once 'sc_dark_weather_functions.php';
+
 // Create a new class
 class SC_Dark_Weather_Check
 {
@@ -8,6 +21,7 @@ class SC_Dark_Weather_Check
   private $sc_long;
   private $sc_lat;
   private $sc_json;
+
   function __construct( $sc_token, $sc_long, $sc_lat, $sc_json )
   {
     $this->sc_token = $sc_token;
@@ -15,37 +29,51 @@ class SC_Dark_Weather_Check
     $this->sc_lat = $sc_lat;
     $this->sc_json = $sc_json;
   }
-  // check if forecast.json exists
-  // check age of forecast.json (<> 30 mins)
-  public function sc_test_json()
+
+  public function sc_test()
   {
+    // array to contain file, token, long, lat, update bool
+    $sc_checked = array();
+
     $file = $this->sc_json;
+    $sc_token = $this->sc_token;
+    $sc_lat = $this->lat;
+    $sc_long = $this->long;
+
+    // check if forecast.json exists
+    // check age of forecast.json (<> 30 mins)
     if (file_exists($file))
     {
       $age = time() - filemtime( $file );
       if ($age > 60*30)
       {
-        return "Need to update forecast.<br>";  // for testing
-        $update = true;
+        // update
+        $sc_checked[0] = $file;
+        $sc_checked[4] = true;
         // exit and update
       }
       else
       {
-        return "No need to update forecast.<br>"; // for testing
-        $update = false;
+          // all is good
+        $sc_checked[0] = $file;
+        $sc_checked[4] = false;
       }
     }
     else
     {
-      return $file . ' is not available. Check the plugin settings.';
+      $sc_checked[0] = 'forecast.json';
+      $sc_checked[4] = true;
     }
   }
-  // END function sc_test_json()
+  // END file test
+
   // check if token has changed
   public function sc_test_token()
   {
+    //if token is the same
     return $this->sc_token;
   }
+
   // Check if latitude or longitude has changed
   public function sc_test_lat_long()
   {
