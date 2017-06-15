03e414fae3082530930720605e9f1b6c4cc2f26a
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-06-14 22:53:40 -0700

    Used Check class a boilerplate for Filter class - maintained commented Check code

--- SC_Dark_Weather_Filter.php
+++ SC_Dark_Weather_Filter.php
@@ -24,7 +24,7 @@
 require_once 'sc_dark_weather_functions.php';
 
 // Create the class
-class SC_Dark_Weather_Filter extends SC_Dark_Weather_Check
+class SC_Dark_Weather_Filter
 {
   private $sc_token;
   private $sc_lat;
@@ -41,86 +41,71 @@ class SC_Dark_Weather_Filter extends SC_Dark_Weather_Check
     $this->sc_json_a = $args[4];
   }
 
-  private function sc_test1()
-  {
-    $update = array(0, 'OK');
 
-    $testArray = array( $this->sc_token, $this->sc_lat, $this->sc_long, $this->sc_json_f, $this->sc_json_a, $update );
+    // public function sc_test()
+    // {
+    //   // array to contain file, token, long, lat, update bool
+    //   $sc_checked = array();
+    //
+    //   $file = $this->sc_json;
+    //   $sc_token = $this->sc_token;
+    //   $sc_lat = $this->lat;
+    //   $sc_long = $this->long;
+    //
+    //   // check if forecast.json exists
+    //   // check age of forecast.json (<> 30 mins)
+    //   if (file_exists($file))
+    //   {
+    //     $age = time() - filemtime( $file );
+    //     if ($age > 60*30)
+    //     {
+    //       // update
+    //       $sc_checked[0] = $file;
+    //       $sc_checked[4] = true;
+    //       // exit and update
+    //     }
+    //     else
+    //     {
+    //         // all is good
+    //       $sc_checked[0] = $file;
+    //       $sc_checked[4] = false;
+    //     }
+    //   }
+    //   else
+    //   {
+    //     $sc_checked[0] = 'forecast.json';
+    //     $sc_checked[4] = true;
+    //   }
+    // }
+    // END file test
 
-    // THIS ARRAY CONTAINS THE FILTERED, COMPARISON READY ARGUMENTS
-    // IT WILL ALSO CONTAIN AN UPDATE ARRAY:
-    // TRUE AND A FILENAME OR FALSE and 'OK'
-    return $testArray;
-  }
+    // check if token has changed
+    // public function sc_test_token()
+    // {
+    //   //if token is the same
+    //   return $this->sc_token;
+    // }
 
-  public function sc_test() {
-    return sc_test1();
-  }
-
-  // public function sc_test()
-  // {
-  //   // array to contain file, token, long, lat, update bool
-  //   $sc_checked = array();
-  //
-  //   $file = $this->sc_json;
-  //   $sc_token = $this->sc_token;
-  //   $sc_lat = $this->lat;
-  //   $sc_long = $this->long;
-  //
-  //   // check if forecast.json exists
-  //   // check age of forecast.json (<> 30 mins)
-  //   if (file_exists($file))
-  //   {
-  //     $age = time() - filemtime( $file );
-  //     if ($age > 60*30)
-  //     {
-  //       // update
-  //       $sc_checked[0] = $file;
-  //       $sc_checked[4] = true;
-  //       // exit and update
-  //     }
-  //     else
-  //     {
-  //         // all is good
-  //       $sc_checked[0] = $file;
-  //       $sc_checked[4] = false;
-  //     }
-  //   }
-  //   else
-  //   {
-  //     $sc_checked[0] = 'forecast.json';
-  //     $sc_checked[4] = true;
-  //   }
-  // }
-  // END file test
-
-  // check if token has changed
-  // public function sc_test_token()
-  // {
-  //   //if token is the same
-  //   return $this->sc_token;
-  // }
-
-  // Check if latitude or longitude has changed
-  // public function sc_test_lat_long()
-  // {
-  //   // If options are set...
-  //   $lat = output_cache('', 'latitude');
-  //   $long = output_cache('', 'longitude');
-  //   // compare construct attributes with plugin values if set
-  //   if ( $lat != $this->sc_lat || $long != $this->sc_long )
-  //   {
-  //     return $lat . ' or ' . $long .' have changed.<br>'; // for testing
-  //     $update = true;
-  //     // exit and update if changed or not set
-  //   }
-  //   else
-  //   {
-  //     return $lat . ', ' . $long .' have not changed.<br>'; // for testing
-  //     $update = false;
-  //   }
-  // }
-  // END function sc_test_lat_long()
-
+    // Check if latitude or longitude has changed
+    // public function sc_test_lat_long()
+    // {
+    //   // If options are set...
+    //   $lat = output_cache('', 'latitude');
+    //   $long = output_cache('', 'longitude');
+    //   // compare construct attributes with plugin values if set
+    //   if ( $lat != $this->sc_lat || $long != $this->sc_long )
+    //   {
+    //     return $lat . ' or ' . $long .' have changed.<br>'; // for testing
+    //     $update = true;
+    //     // exit and update if changed or not set
+    //   }
+    //   else
+    //   {
+    //     return $lat . ', ' . $long .' have not changed.<br>'; // for testing
+    //     $update = false;
+    //   }
+    // }
+    // END function sc_test_lat_long()
+  
 }
-//class SC_Dark_Weather_Check
+// class SC_Dark_Weather_Filter
