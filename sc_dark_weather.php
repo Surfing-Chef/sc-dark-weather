6748d815832b5d9ec1095f61651dad58abdca9b6
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-06-18 15:32:55 -0700

    Edited description in comment block

--- sc_dark_weather.php
+++ sc_dark_weather.php
@@ -1,15 +1,16 @@
 <?php
-/*
-Plugin Name: SC Dark Weather
-Plugin URI: https://github.com/Surfing-Chef/Dark
-Description: Uses a wrapper to display a weather feed from Darksky.net
-Version: 1.5
-Author: Surfing-Chef
-Author URI: https://github.com/Surfing-Chef
-License: GPL3
-License URI: https://www.gnu.org/licenses/gpl-3.0.html
-Text Domain: sc-dark-weather
+/**
+ * Plugin Name: SC Dark Weather
+ * Plugin URI: https://github.com/Surfing-Chef/Dark
+ * Description: Uses a wrapper to display a weather feed from Darksky.net
+ * Version: 1.5
+ * Author: Surfing-Chef
+ * Author URI: https://github.com/Surfing-Chef
+ * License: GPL3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: sc-dark-weather
 */
+
 // Exit if accessed directly
 defined( 'ABSPATH' ) or die( "Error: contact admin@surfing-chef.com" );
 
