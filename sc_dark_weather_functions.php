d91b2c948cb8cd47f57a10436b7d8a7bbd309337
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-06-18 22:39:23 -0700

    Created function to retrieve address based on plugin latitude and longitude options

--- sc_dark_weather_functions.php
+++ sc_dark_weather_functions.php
@@ -85,3 +85,33 @@ function getDirection($bearing)
  }
  return $direction;
 }
+
+// get address
+function googleApiAddress( $lat, $long )
+{
+  $url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&sensor=false";
+
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_HEADER, false);
+  curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_ENCODING, "");
+  $curlData = curl_exec($curl);
+  curl_close($curl);
+
+  $address = json_decode($curlData);
+
+  $obj = $address->results;
+  $obj = $obj[0]->address_components;
+  $locale = $obj[1];
+  $locale = $locale->long_name;
+  $region = $obj[3];
+  $region = $region->short_name;
+  $country = $obj[4];
+  $country = $country->long_name;
+
+  $address = $locale . ', ' . $region . ', ' . $country;
+
+  return $address;
+}
